02d2de6171ce049a34053bd4a3a0450ee2db1848
HamiHame <33611529+gabrielcanali@users.noreply.github.com>
2020-02-29 08:06:38 -0300

    Update index.py

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ async def on_ready():
 
 @cliente_discord.event
 async def on_message(texto):
-    if (texto.author.id == '683114702510882889'): return
+    if (texto.author.id == 'Id'): return
     if texto.content.startswith('Bot'): #Se uma frase tiver Bot no inico
         frase = texto.content[3:].strip() #Tira 'Bot' da frase
         if frase.lower().startswith('dado'): #ATira dado de 1 a 6
@@ -23,4 +23,4 @@ async def on_message(texto):
                         resposta = random.choice(['Não respondo a isso','Sim','As vezes','Não','Claro','NUNCA!','Um dia talvez','A resposta está dentro de ti','Mais ou menos','Uma Bosta','Podia ser pior'])
                         await cliente_discord.send(texto.channel,resposta)
                         return
-cliente_discord.run('NjgzMTE0NzAyNTEwODgyODg5.Xlm20A.xC5LZU28SiZiN4Z9dkF6V6yG_VM')
+cliente_discord.run('Token')
